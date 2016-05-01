5a06edc27e64ab10addb56f423b98eb03370eb30
Vetriti <relife@inbox.ru>
2016-05-02 03:15:46 +0400

    Форма добавления пользователя

---
+++ AddUserForm.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace pctesting
+{
+    partial class AddUserForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.userNameLabel = new System.Windows.Forms.Label();
+            this.userNameTextBox = new System.Windows.Forms.TextBox();
+            this.userPasswordLabel = new System.Windows.Forms.Label();
+            this.userPasswordTextBox = new System.Windows.Forms.TextBox();
+            this.saveButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // userNameLabel
+            // 
+            this.userNameLabel.AutoSize = true;
+            this.userNameLabel.Location = new System.Drawing.Point(12, 9);
+            this.userNameLabel.Name = "userNameLabel";
+            this.userNameLabel.Size = new System.Drawing.Size(135, 17);
+            this.userNameLabel.TabIndex = 0;
+            this.userNameLabel.Text = "Имя пользователя:";
+            // 
+            // userNameTextBox
+            // 
+            this.userNameTextBox.Location = new System.Drawing.Point(15, 29);
+            this.userNameTextBox.Name = "userNameTextBox";
+            this.userNameTextBox.Size = new System.Drawing.Size(251, 22);
+            this.userNameTextBox.TabIndex = 1;
+            // 
+            // userPasswordLabel
+            // 
+            this.userPasswordLabel.AutoSize = true;
+            this.userPasswordLabel.Location = new System.Drawing.Point(12, 54);
+            this.userPasswordLabel.Name = "userPasswordLabel";
+            this.userPasswordLabel.Size = new System.Drawing.Size(61, 17);
+            this.userPasswordLabel.TabIndex = 2;
+            this.userPasswordLabel.Text = "Пароль:";
+            // 
+            // userPasswordTextBox
+            // 
+            this.userPasswordTextBox.Location = new System.Drawing.Point(15, 74);
+            this.userPasswordTextBox.Name = "userPasswordTextBox";
+            this.userPasswordTextBox.PasswordChar = '*';
+            this.userPasswordTextBox.Size = new System.Drawing.Size(251, 22);
+            this.userPasswordTextBox.TabIndex = 3;
+            // 
+            // saveButton
+            // 
+            this.saveButton.Location = new System.Drawing.Point(166, 118);
+            this.saveButton.Name = "saveButton";
+            this.saveButton.Size = new System.Drawing.Size(100, 23);
+            this.saveButton.TabIndex = 4;
+            this.saveButton.Text = "Сохранить";
+            this.saveButton.UseVisualStyleBackColor = true;
+            this.saveButton.Click += new System.EventHandler(this.saveButton_Click);
+            // 
+            // AddUserForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(282, 153);
+            this.Controls.Add(this.saveButton);
+            this.Controls.Add(this.userPasswordTextBox);
+            this.Controls.Add(this.userPasswordLabel);
+            this.Controls.Add(this.userNameTextBox);
+            this.Controls.Add(this.userNameLabel);
+            this.Name = "AddUserForm";
+            this.Text = "AddUserForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label userNameLabel;
+        private System.Windows.Forms.TextBox userNameTextBox;
+        private System.Windows.Forms.Label userPasswordLabel;
+        private System.Windows.Forms.TextBox userPasswordTextBox;
+        private System.Windows.Forms.Button saveButton;
+    }
+}
---
+++ AddUserForm.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using pctesting.DBService;
+
+namespace pctesting
+{
+    public partial class AddUserForm : Form
+    {
+        public AddUserForm()
+        {
+            InitializeComponent();
+        }
+
+        private void saveButton_Click(object sender, EventArgs e)
+        {
+            DBService.DataServiceClient client = new DataServiceClient();
+            if(client.addUser(userNameTextBox.Text, userPasswordTextBox.Text))
+            {
+                MessageBox.Show("Пользователь успешно добавлен", "Пользователь добавлен");
+            }
+            else
+            {
+                MessageBox.Show("Возникла ошибка добавления пользователя!\nВозможно пользователь с таким именем уже существует.", "Ошибка добавления", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
