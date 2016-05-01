e32e51ebede99a33e30b3766944566d5c6a0af9d
Vetriti <relife@inbox.ru>
2016-05-01 22:18:19 +0400

    Добавил класс для отчётов

---
+++ MyService/Report.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using iTextSharp.text;
+using iTextSharp;
+using System.IO;
+using iTextSharp.text.pdf;
+
+
+namespace pctesting
+{
+    class Report
+    {
+        
+    }
+}
